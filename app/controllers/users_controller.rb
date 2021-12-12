1a7bf3f1d44bc2fceb7bd80322e36ca6d170787f
Jason Ho <jason.ho@obf.ateneo.edu>
2021-12-12 21:54:27 +0800

    show user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,42 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user_id
+  before_action :check_admin
+  before_action :set_user, only: %i[show edit update]
+
   def index
-    if current_user.admin
-      @users = User.where(admin: false).order('created_at DESC')
-    else
-      redirect_to root_path
-    end
+    @users = User.where(admin: false).order('created_at DESC')
+  end
+
+  def show; end
+
+  def new; end
+
+  def create; end
+
+  def edit; end
+
+  def update; end
+
+  private
+
+  def set_user_id
+    @user_id = current_user.id
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def check_admin
+    redirect_to root_path if !current_user.admin
+  end
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params
+      .require(:user)
+      .permit(:email, :password, :password_confirmation, :admin, :approved)
+      .merge(user_id: @user_id)
   end
 end
