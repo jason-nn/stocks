d88bfe8072651feac100159445be9a6e234177e3
Jason Ho <jason.ho@obf.ateneo.edu>
2021-12-12 18:45:07 +0800

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = User.create(email: 'jason@admin.com', password: 'password', admin: true)
+admin.transactions.create(action: 'cash in', amount: 1000)
+jason =
+  User.create(
+    email: 'jason.ho@obf.ateneo.edu',
+    password: 'password',
+    admin: false,
+  )
+jason.transactions.create(action: 'cash in', amount: 2000)
+jason.transactions.create(
+  action: 'purchase',
+  amount: -420.69,
+  stock: 'TSLA',
+  quantity: 2,
+)
+jason.transactions.create(
+  action: 'sale',
+  amount: +690,
+  stock: 'TSLA',
+  quantity: 1,
+)
