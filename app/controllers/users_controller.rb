20469434f2a4f88ba03cd6fe085e8b2e2281af74
Jason Ho <jason.ho@obf.ateneo.edu>
2021-12-13 12:42:46 +0800

    account details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user_id
   before_action :check_admin, only: %i[index show new create edit update]
+  before_action :check_non_admin, only: %i[account]
   before_action :set_user, only: %i[show edit update]
 
   def index
@@ -20,6 +21,7 @@ class UsersController < ApplicationController
 
   def account
     @balance = Transaction.where(user_id: @user_id).pluck(:amount).sum
+    @user = current_user
   end
 
   private
@@ -34,6 +36,10 @@ class UsersController < ApplicationController
 
   def check_admin
     redirect_to root_path if !current_user.admin
+  end
+
+  def check_non_admin
+    redirect_to root_path if current_user.admin
   end
 
   # Only allow a list of trusted parameters through.
