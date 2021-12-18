8d221c798b9bf881e4fdace54c0e44fafa21570c
Jason Ho <jason.ho@obf.ateneo.edu>
2021-12-18 15:23:01 +0800

    re added names

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,7 @@ class UsersController < ApplicationController
   def approve
     @user = User.find(params[:id])
     @user.update(approved: true)
-    redirect_to users_path, notice: @user.email + ' has been approved'
+    redirect_to users_path, notice: @user.name + ' has been approved'
   end
 
   def portfolio; end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,5 @@ class User < ApplicationRecord
 
   has_many :transactions
 
-  validates :name, uniqueness: true, allow_nil: true
+  validates :name, presence: true, uniqueness: true, allow_nil: true
 end
